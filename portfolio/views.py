a019c6d0bd769d291ab35b2bac4918b4de305a70
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2023-04-15 19:48:33 +0530

    initial commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -13,6 +13,9 @@ def home(request):
 def about(request):
 	return render(request, 'about.html')
 
+def resume(request):
+	return render(request, 'resume.html')
+
 def handleBlog(request):
 	posts = Blog.objects.all()
 	context = {'posts': posts}
